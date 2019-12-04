cfcf99a00aa5176d73f5ba0a0f4cc9e81b58c198
IshibaSeiko <seiko.ishiba@st-ventures.jp>
2019-12-04 17:25:19 +0900

    プロジェクトを作成

---
+++ LocationInfo/LocationInfo/LocationInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LocationInfoViewController.swift
+//  LocationInfo
+//
+//  Created by STV-M025 on 2019/12/04.
+//  Copyright © 2019 STV-M025. All rights reserved.
+//
+
+import UIKit
+
+class LocationInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
